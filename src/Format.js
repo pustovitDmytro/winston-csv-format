13eedf05436d628f1fd537dc2ff062770bd7877d
dmytroPust <dipustovit@gmail.com>
2020-05-07 23:15:54 +0300

    New: adds missed options to configuration

--- src/Format.js
+++ src/Format.js
@@ -2,13 +2,13 @@ import { format } from 'winston';
 
 export default function CSVFormatter(fields, options = {}) {
     const delim = options.delimiter || ';';
-
+    const missed = options.missed || ''
 
     return format.printf(logObject => {
         const data = fields.map(key => {
             const value = logObject[key];
 
-            return value === undefined ? '' : value;
+            return value === undefined ? missed : value;
         });
 
         return data.join(delim);
